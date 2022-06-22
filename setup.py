b94647270def6326eb723a4040a94463b743c9c4
Raymond Liu <rayliu25@gmail.com>
2022-06-21 20:08:28 -0400

    new commits

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='rayliu25@gmail.com',
+      license='MIT',
+      packages=['funniest3535'],
+      zip_safe=False)
